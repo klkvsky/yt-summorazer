238467c8b381cb40439bbb8be81a94af3638f9e5
Kulikovsky <80684329+klkvsky@users.noreply.github.com>
2023-02-25 07:55:56 +0300

    v0.9

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,60 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NextResponse, NextRequest } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+
+  console.log("started");
+
+  const res = await fetch(`https://youtubetranscript.com/?server_vid=${id}`);
+  const data = await res.text();
+
+  let fullText = "";
+  const textRegex = /<text.*?>(.*?)<\/text>/gm;
+  let match;
+  while ((match = textRegex.exec(data)) !== null) {
+    fullText += match[1] + " ";
+  }
+  const generatedSum = await generate(fullText);
+
+  console.log(generatedSum);
+
+  return NextResponse.json({ generatedSum });
+}
+
+async function generate(text: string) {
+  console.log("started 2");
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(text.slice(0, 4000)),
+      temperature: 0,
+      max_tokens: 500,
+    });
+    return completion.data.choices[0].text;
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      // res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      // res.status(500).json({
+      //   error: {
+      //     message: "An error occurred during your request.",
+      //   },
+      // });
+    }
+  }
+}
+
+function generatePrompt(text: string) {
+  console.log("started 3");
+  // return `return string with summarization of next transcription: ${text} in 5 paragraphs`;
+  return `I want you to act as an essay writer. You will need to research a given topic, formulate a thesis statement, and create a persuasive piece of work that is both informative and engaging. My first suggestion request is “I need to summoraze transcript of youtube video: ${text} under 2489 characters`;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,95 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import { Inter } from "next/font/google";
+import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [link, setLink] = useState<string>();
+  const [isLinkValid, setIsLinkValid] = useState<boolean>(true);
+  const [text, setText] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  //
+  async function getTranscription() {
+    if (link) {
+      const textToAdd = extractYouTubeId(link);
+      const url = `/api/hello?id=${textToAdd}`;
+
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setText(data.generatedSum), setIsLoading(false);
+        })
+        .catch((error) => console.error(error));
+    }
+  }
+  //
+  //
+  async function getLink() {
+    if (document) {
+      navigator.clipboard
+        .readText()
+        .then((clipText) =>
+          isYoutubeLink(clipText)
+            ? setLink(clipText)
+            : console.error("it's not a youtube link")
+        );
+    }
+  }
+  //
+  //
+  function isYoutubeLink(text: string): boolean {
+    const youtubeRegex =
+      /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}$/i;
+    if (!youtubeRegex.test(text)) {
+      setIsLinkValid(false);
+    }
+    return youtubeRegex.test(text);
+  }
+  function extractYouTubeId(url: string): string | null {
+    const regex =
+      /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=))([\w-]{11})(?:\S+)?$/;
+    const match = url.match(regex);
+    return match?.[1] || null;
+  }
+  //
+  //
+  useEffect(() => {
+    if (!isLinkValid) {
+      setTimeout(() => {
+        setIsLinkValid(true);
+      }, 1500);
+    }
+  }, [isLinkValid]);
+
+  useEffect(() => {
+    if (link) {
+      setIsLoading(true);
+      getTranscription();
+    }
+  }, [link]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <main className="bg-white min-h-screen w-screen text-black text-center flex flex-col items-center gap-[20px] pt-[20vh]">
+        <button
+          onClick={() => {
+            getTranscription();
+          }}
+          className="hidden"
+        >
+          get me
+        </button>
+
+        <a
+          href="htpps://github.com/"
+          className="rounded-full border border-neutral-800 h-[44px] gap-2 px-3 hover:scale-[1.04] transition-all duration-300 ease-in-out cursor-pointer flex flex-row items-center group relative focus:scale-[1.04] hover:bg-black hover:text-white"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -35,60 +107,92 @@ export default function Home() {
           </span>
         </a>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <h1 className="text-4xl lg:text-5xl font-bold max-w-lg">
+          Quickly Summarize YouTube Videos Using Transcriptions
+        </h1>
+
+        <p className="hidden">47,118 videos summorized so far.</p>
+
+        <div className="max-w-xl w-full">
+          <div className="flex mt-2 lg:mt-5 items-center space-x-3">
+            <button
+              className={` text-white px-4 py-2 rounded-[10px] flex flex-row items-center justify-center gap-2 font-medium text-lg mx-auto    transition-all duration-300 ease-in-out shadow-md hover:shadow-lg active:shadow-sm outline-2 focus:shadow-lg  ${
+                !isLoading &&
+                (isLinkValid
+                  ? "bg-black/80 hover:bg-black focus:bg-black focus:scale-[1.04] active:scale-[0.96] hover:scale-[1.04]"
+                  : "bg-red-500")
+              } ${isLoading && "bg-black/50 scale-[1.2]"}`}
+              onClick={() => {
+                getLink();
+              }}
+            >
+              {isLoading && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 animate-spin"
+                >
+                  <circle cx="12" cy="20" r="2"></circle>
+                  <circle cx="12" cy="4" r="2"></circle>
+                  <circle cx="6.343" cy="17.657" r="2"></circle>
+                  <circle cx="17.657" cy="6.343" r="2"></circle>
+                  <circle cx="4" cy="12" r="2.001"></circle>
+                  <circle cx="20" cy="12" r="2"></circle>
+                  <circle cx="6.343" cy="6.344" r="2"></circle>
+                  <circle cx="17.657" cy="17.658" r="2"></circle>
+                </svg>
+              )}
+              {!isLoading &&
+                (isLinkValid ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-5 h-5 "
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 01-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0113.5 1.5H15a3 3 0 012.663 1.618zM12 4.5A1.5 1.5 0 0113.5 3H15a1.5 1.5 0 011.5 1.5H12z"
+                      clipRule="evenodd"
+                    />
+                    <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 019 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0116.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625v-12z" />
+                    <path d="M10.5 10.5a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963 5.23 5.23 0 00-3.434-1.279h-1.875a.375.375 0 01-.375-.375V10.5z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ))}
+              <span>
+                {!isLoading && (isLinkValid ? "Paste link" : "Invalid link")}
+                {isLoading && "Loading"}
+              </span>
+            </button>
+          </div>
+
+          <p
+            className="lg:shadow-xl px-6 py-4 text-left my-10 prose max-w-prose transition-all duration-500 ease-in-out shadow-md mx-4"
+            style={{
+              opacity: text ? 1 : 0,
+              transform: `translate(${0}px, ${text ? 0 : 200}px)`,
+            }}
+          >
+            {text}
+          </p>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
